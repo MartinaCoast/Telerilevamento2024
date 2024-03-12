a27d0895069d3a348c2e5dd7a2c4cf4cffffde32
MartinaCoast <162603285+MartinaCoast@users.noreply.github.com>
2024-03-12 17:42:23 +0100

    Update 03_visualisation_temp.R

--- 03_visualisation_temp.R
+++ 03_visualisation_temp.R
@@ -1,1 +1,12 @@
+# Satellite data visualisation in R by imageRy
+
+library(terra)
+library(imageRy)
+
+# list of data available in imageRy
+im.list()
+
+# Importing data
+# assegnamo all'importazione il nome mato per semplicficare
+mato <- im.import("matogrosso_ast_2006209_lrg.jpg")
 
