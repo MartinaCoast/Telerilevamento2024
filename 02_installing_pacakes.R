84209e65ed1aa5d0deb0712e11c16f302e477b19
MartinaCoast <162603285+MartinaCoast@users.noreply.github.com>
2024-03-12 17:31:43 +0100

    Update 02_installing_pacakes.R

--- 02_installing_pacakes.R
+++ 02_installing_pacakes.R
@@ -1,4 +1,13 @@
 # Install new packages in R
 
 install.packages("terra")
-library(terra)
+library(terra) # or require()
+
+install.packages("devtools")
+library(devtools)
+
+# install the imageRy package from GitHub
+# a devtools function:
+install_github("ducciorocchini/imageRy")
+library(imageRy)
+
