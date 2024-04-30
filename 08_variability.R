cdeda1df970bdfce3afba09c199af00c47a8b25e
MartinaCoast <162603285+MartinaCoast@users.noreply.github.com>
2024-04-30 17:29:07 +0200

    Update 08_variability.R

--- 08_variability.R
+++ 08_variability.R
@@ -22,3 +22,17 @@ sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd) # con focal ...
 plot(sd3)
 
 install.packages("viridis")
+
+# in viridis esistono delle palette già formate, posso scegliere direttamente quelle: noi usiamo quella viridis
+viridisc <- colorRampPalette(viridis(7))(100) # il 7 vuol dire che è la settima di una serie di palette, qui assegniamo la palette viridis(7) all'oggetto viridisc
+plot(sd3, col=viridisc)
+
+# calculate the sd mw of 7 pixels
+sd7 <- focal(nir, matrix(1/49, 7, 7), fun=sd)
+plot(sd7, col=viridisc)
+
+sd13 <- focal(nir, matrix(1/169, 13, 13), fun=sd) # calcolo la deviazione standard 13x13
+plot(sd13, col=viridisc) # ho un effetto iper sfocato ma non ho capito perche e cosa vuol dire questa roba
+
+sdstack <- c(sd3, sd7, sd13)
+plot(sdstack, col=viridisc)
