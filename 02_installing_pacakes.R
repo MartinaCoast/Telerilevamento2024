36829da46165964758d3ff066ad9a7ff7fcd39b5
MartinaCoast <162603285+MartinaCoast@users.noreply.github.com>
2025-01-15 10:34:59 +0100

    Update 02_installing_pacakes.R

--- 02_installing_pacakes.R
+++ 02_installing_pacakes.R
@@ -1,12 +1,14 @@
-# Install new packages in R
+# Install new packages in R: per vedere i pacchetti disponibili posso cercare su CRAN, ogni pacchetto ha la sua descrizione, oppure posso installare pacchetti anche da GitHub
 
-install.packages("terra")
-library(terra) # or require()
+install.packages("terra") #il nome del pacchetto è tra virgolette perchè sto uscendo da R per ottenere il pacchetto
+library(terra) # or require(): questa funzione mi serve per richiamare il pacchetto e verificare che sia stato installato e mi dice che versione ho installato
 
-install.packages("devtools")
+install.packages("devtools") #questo pacchetto mi serve per poter scaricare il prossimo
 library(devtools)
 
-# install the imageRy package from GitHub
+#se volessi rimuovere un pacchetto dovrei usare ala funzione remove.packages("")
+
+# install the imageRy package from GitHub: è un pacchetto che serve per scaricare più velocemente le immagini
 # a devtools function:
 install_github("ducciorocchini/imageRy")
 library(imageRy)
