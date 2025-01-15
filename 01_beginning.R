feb28359b4039a7966d1eaa62542ca98890d6b1e
MartinaCoast <162603285+MartinaCoast@users.noreply.github.com>
2025-01-15 10:16:21 +0100

    Update 01_beginning.R

--- 01_beginning.R
+++ 01_beginning.R
@@ -14,14 +14,18 @@ flowers
 insects <- c(10,16,25,42,61,73)
 insects
 
-plot(flowers, insects)
+plot(flowers, insects) #è una funzione, quindi è come se scrivessi f(x,y)
 
 # changing plot parameters
 
-#symbols
+#symbols: posso cambiare il simbolo, esiste un codice per ogni tipo di simbolo, pch = point character
 plot(flowers, insects,pch=19)
 
-#symbol dimensions
-plot(flowers, insects, pch=19, cex=2)
+#symbol dimensions: posso modificare anche le dimensioni dei punti del plot, cex = character exageration
+plot(flowers, insects, pch=19, cex=2)  #se voglio rimpicciolire metto numeri più piccoli
 
+# per cambiare il colore del simbolo aggiungo tra parentesi col="nome del colore"
 plot(flowers, insects, pch=19, cex=2, cex=.5, col="chocolate1")
+
+#se voglio cambiare i nomi di asse x e y aggiungo xlab="nuovo nome" e ylab="nuovo nome"
+plot(flowers, insects, pch=19, cex=2, cex=.5, col="chocolate1", xlab="flora", ylab="fauna")
