e7345a2f09ec2dd65e294b5a2756cdafbed51359
MartinaCoast <162603285+MartinaCoast@users.noreply.github.com>
2025-01-18 17:05:32 +0100

    Create 00_idea_progetto.R

---
+++ 00_idea_progetto.R
@@ -0,0 +1,12 @@
+#IDEA PROGETTO
+
+#zona dietro casa nel 2001 quando ci siamo trasferiti
+#zona dietro casa subito dopo la costruzione del museo
+#zona dietro casa  2024
+#focus su prato di fianco a casa, prima della tesi e adesso
+
+#colori viridis cosi faccio vedere anche a marci
+
+
+
+
