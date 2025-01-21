2dc92ef678d27811a299a4292d17c193356c6e4e
MartinaCoast <162603285+MartinaCoast@users.noreply.github.com>
2025-01-21 13:09:07 +0100

    Update 00_idea_progetto.R

--- 00_idea_progetto.R
+++ 00_idea_progetto.R
@@ -15,12 +15,14 @@ library(imageRy) #mi serve per fare la classificazione e il plotting delle immag
 
 setwd("C:/Users/Acer/Documents/UNIBO/MAGISTRALE/telerilevamento geo-ecologico/2017-2024")
 
+#mettiamo banda 1=rosso, banda 2 è verde, banda 3 è blu, banda 4 è nir
 r17 <- rast("r17.tiff")
 g17 <- rast("g17.tiff")
 b17 <- rast("b17.tiff")
 nir17 <- rast("nir17.tiff")
 
-m2017 <- c(r17, g17, b17, nir17)
+#cosi adesso ho l'immagine in true color data dalla sovrapposizione delle varie bande
+m2017 <- c(r17, g17, b17, nir17) #sovrappongo le varie bande nell'ordine cui sopra
 
 r24 <- rast("r24.tiff")
 g24 <- rast("g24.tiff")
@@ -32,6 +34,17 @@ m2024 <- c(r24, g24, b24, nir24)
 par(mfrow=c(1,2))
 im.plotRGB(m2017, 1,2,3)
 im.plotRGB(m2024, 1,2,3)
+#vsualizzo le immagini facendole corrispondere a ogni banda il proprio colore=> banda rossa verrà colorata di rosso ecc
+#però qui non noto troppo le differenze, quindi dato che mi interessa il cambiamento della vegetazione, proviamo a scambiare le bande visualizzando la banda del nir in rosso
+
+dev.off()
+
+par(mfrow=c(2,2))
+im.plotRGB(m2017, 1,2,3) 
+im.plotRGB(m2017, 4,2,3) #cosi metto a confronto le due immagini dello stesso anno ma con l'infrarosso nel secondo caso
+im.plotRGB(m2024, 1,2,3)
+im.plotRGB(m2024, 4,2,3) #stessa cosa
+
 
 
 
