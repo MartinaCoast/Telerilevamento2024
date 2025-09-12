ac800cbaa62e933278416b203caeec92092f4175
MartinaCoast <162603285+MartinaCoast@users.noreply.github.com>
2025-09-12 12:37:30 +0200

    Update Progetto esame_Blatten.R

--- Progetto esame_Blatten.R
+++ Progetto esame_Blatten.R
@@ -37,36 +37,51 @@ library(imageRy)
 # Includo poi tutte le bande in uno stack per creare un'immagine unica per entrambi gli anni
 
 #2024
-g24_2 <- rast("24_b2.tiff")
-g24_3 <- rast("24_b3.tiff")
-g24_4 <- rast("24_b4.tiff")
-g24_8 <- rast("24_b8.tiff")
-G24 <- c(g24_2, g24_3, g24_4, g24_8)
+g24_4 <- rast("24_b4.tiff") #red
+g24_3 <- rast("24_b3.tiff") #green
+g24_2 <- rast("24_b2.tiff") #blue
+g24_8 <- rast("24_b8.tiff") #nir
+G24 <- c(g24_4, g24_3, g24_2, g24_8)
 
 #2025
-g25_2 <- rast("25_b2.tiff")
-g25_3 <- rast("25_b3.tiff")
-g25_4 <- rast("25_b4.tiff")
-g25_8 <- rast("25_b8.tiff")
-G25 <- c(g25_2, g25_3, g25_4, g25_8)
+g25_4 <- rast("25_b4.tiff") #red
+g25_3 <- rast("25_b3.tiff") #green
+g25_2 <- rast("25_b2.tiff") #blue
+g25_8 <- rast("25_b8.tiff") #nir
+G25 <- c(g25_4, g25_3, g25_2, g25_8)
 
 
 # Imposto un multiframe per visualizzare le due immagini in True Color tramite la funzione par() di imageRy
 # Creo una griglia di 1 riga e 2 colonne, aggiungo anche i titoli relativi agli anni
 # Per le immagini seguo la seguente sequenza di bande: r=1, g=2, b=3
 par(mfrow=c(1,2))
-im.plotRGB(G24, 1,2,3,title="2024")
-im.plotRGB(G25, 1,2,3,title="2025")
+im.plotRGB(G24, 1,2,3)
+title("2024")
+im.plotRGB(G25, 1,2,3)
+title("2025")
 
 dev.off()
 
 # Creo un secondo multiframe mettendo il NIR al posto del rosso in modo da evidenziare meglio l'impatto del crollo sulla vegetazione
 # In questo modo tutto ciò che riflette il NIR (la vegetazione) risulterà rosso
-par(mfrow(1,2))
-im.plotRGB(G24, 4,2,3,title="2024")
-im.plotRGB(G25, 4,2,3,title="2025")
+par(mfrow=c(1,2))
+im.plotRGB(G24, 4,2,3)
+title("2024")
+im.plotRGB(G25, 4,2,3)
+title("2025")
 
 dev.off()
+
+###### PROVAAAAAA NON CAPISCO DOVE TROVO IMMAGINE IN TRUE COLOR
+par(mfrow=c(3,2))
+im.plotRGB(G24, 4,2,3)
+im.plotRGB(G25, 4,2,3)
+im.plotRGB(G24, 4,3,2)
+im.plotRGB(G25, 4,3,2)
+im.plotRGB(G24, 4,1,2)
+im.plotRGB(G25, 4,1,2)
+####
+
 
 # Calcolo innanzitutto l'NDVI (Normalized Difference Vegetation Index) per entrambi gli anni seguendo la formula:
 # NDVI = (nir-red)/(nir+red)
